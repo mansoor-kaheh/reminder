93cd7c4982b36855793052adb01c5fdb309e2944
Mansoor Kaheh <mansoor.kaheh@gmail.com>
2024-03-28 22:15:28 -0400

    remove unused import

--- src/main/java/com/devmantech/reminders/config/MapperConfig.java
+++ src/main/java/com/devmantech/reminders/config/MapperConfig.java
@@ -1,6 +1,5 @@
 package com.devmantech.reminders.config;
 
-import com.devmantech.reminders.mapper.ReminderMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
